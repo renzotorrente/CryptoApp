f5e1ab0189b15aeb075f7ba23177234c4db96964
RenzoTorrente <renzotor7@hotmail.com>
2021-10-28 17:11:33 -0300

    creation of the tab menu

--- App.js
+++ App.js
@@ -2,17 +2,47 @@
 import React from 'react';
 import {NavigationContainer}  from '@react-navigation/native'
 import CoinStack from './src/components/CoinStack';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Image, StyleSheet } from 'react-native';
+import colors from './src/res/colors';
 
 const App = () => {
-
+const Tabs = createBottomTabNavigator();
   return (
    <NavigationContainer>
-   <CoinStack/>
+       <Tabs.Navigator
+        screenOptions={({ route }) => ({
+          tabBarActiveTintColor: "#ffff",
+          tabBarInactiveTintColor: "orange",
+          tabBarLabelStyle:{
+            fontSize: 13,
+            marginBottom:2
+          },
+          tabBarStyle: {
+           backgroundColor: colors.blackPearl
+          },
+
+        })}
+      >
+       <Tabs.Screen  
+        name="coins"
+       component={CoinStack}
+       options={{
+         tabBarIcon:({size, color})=>(
+           <Image style={{tintColor:color ,width:size, height:size}} 
+           source={require('../CryptoApp/src/assets/bank.png')} />
+         ),
+       
+       }}
+/>
+     </Tabs.Navigator>
+  
    </NavigationContainer>
 
   );
 };
+
+
 
 
 
